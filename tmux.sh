e0ba7a2461657e34733fe5305aee1636c7cb1616
pengyongsheng <pys1024@sina.com>
2024-06-17 11:36:44 +0800

    feat: support for multi sessions

--- tmux.sh
+++ tmux.sh
@@ -2,28 +2,30 @@
 
 cd
 tmux new-session -d -s pys1024
+tmux rename-window Home
 tmux split-window -h
 tmux select-pane -t 1
 tmux split-window -v
 tmux select-pane -t 1
 tmux send-keys "htop" C-m
 tmux select-pane -t 2
-tmux send-keys "cd clashy" C-m
-tmux send-keys "./Clashy*" C-m
-tmux send-keys C-c
-tmux send-keys "./Clashy*" C-m
+# tmux send-keys "cd clashy && ./Clashy*; ./run.sh" C-m
 tmux select-pane -t 3
 
 tmux new-window
+tmux rename-window Code
 tmux split-window -h
 tmux select-pane -t 1
 tmux split-window -v
+tmux select-pane -t 3
 
 tmux new-window
+tmux rename-window Debug
 tmux split-window -h
 tmux select-pane -t 1
 tmux split-window -v
+tmux select-pane -t 3
 
 tmux next-window
 
-
+tmux a
